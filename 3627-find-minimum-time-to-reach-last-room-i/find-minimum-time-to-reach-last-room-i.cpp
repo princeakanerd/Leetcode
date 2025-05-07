e685c484fccc575353ca8a13419268cf5b7300f1
Prince Gajnani <132270009+princeakanerd@users.noreply.github.com>
2025-05-08 00:27:38 +0530

    Time: 51 ms (13.33%) | Memory: 37.6 MB (7.53%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isvalid(int i, int j, int m, int n) {
+        return i >= 0 && j >= 0 && i < m && j < n;
+    }
+
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size(), n = moveTime[0].size();
+        vector<vector<int>> distance(m, vector<int>(n, INT_MAX));
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+
+        pq.push({0, {0, 0}});
+        vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        distance[0][0] = 0;
+        while (!pq.empty()) {
+            int currdist = pq.top().first;
+            int i = pq.top().second.first, j = pq.top().second.second;
+            pq.pop() ;
+            for (auto it : dir) {
+                int newi = i + it[0], newj = j + it[1];
+                if (isvalid(newi, newj, m, n) && distance[newi][newj] >currdist + max(0, moveTime[newi][newj] - currdist) + 1) {
+                    distance[newi][newj] = currdist + max(0, moveTime[newi][newj] - currdist) + 1 ;
+                    pq.push({distance[newi][newj] , {newi , newj}}) ;
+                }
+            }
+        }
+
+        return distance[m-1][n - 1] ;
+    }
+};
