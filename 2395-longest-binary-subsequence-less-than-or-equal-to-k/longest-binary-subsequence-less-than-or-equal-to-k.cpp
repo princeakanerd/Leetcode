96a72273e8097adf6a1989888d1981058693c43c
Prince Gajnani <132270009+princeakanerd@users.noreply.github.com>
2025-06-26 14:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (33.33%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    bool check(int idx , string &s , int k) {
-        int n = s.length() ;
-        long long mul = 1 ;
-        long long num = 0 ;
-        for(int i = n - 1 ;i >= idx ; i--) {
-                if(s[i] == '1'){
-                    num += mul ;
-                }
-                if(num > k || mul > k) return false ;
-                mul *= 2;
-        }
-        return true ;
-    }
+    // bool check(int idx , string &s , int k) {
+    //     int n = s.length() ;
+    //     long long mul = 1 ;
+    //     long long num = 0 ;
+    //     for(int i = n - 1 ;i >= idx ; i--) {
+    //             if(s[i] == '1'){
+    //                 num += mul ;
+    //             }
+    //             if(num > k || mul > k) return false ;
+    //             mul *= 2;
+    //     }
+    //     return true ;
+    // }
     int longestSubsequence(string s, int k) {
         int cnt = s.length() ;
         int n = s.length() ;
-        for(int i = 0 ;i < n ; i++ ){
+        long long mul = 1 , num = 0 ;
+        int idx = -1 ;
+        for(int i = n - 1 ;i >= 0 ; i-- ){
             if(s[i] == '1') {
-                if(!check(i ,s ,k)) cnt -- ;
-                else return cnt;
+                num += mul ;
             }
+            if(mul > k || num > k) {idx = i ;break ;}
+            mul *= 2;
+
+            
         }    
+
+        for(int i = 0 ;i <= idx ; i++ ){
+            cnt -= (s[i] == '1') ;
+        }
+        
         return cnt ;
     }
 };
