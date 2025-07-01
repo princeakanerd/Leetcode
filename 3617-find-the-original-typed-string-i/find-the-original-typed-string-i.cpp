a0a04d837e342b21ddfe0fba828d5994b616d14b
Prince Gajnani <132270009+princeakanerd@users.noreply.github.com>
2025-07-01 10:06:23 +0530

    Time: 5 ms (27.17%) | Memory: 9 MB (10.92%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int ans = 1 ;
+
+        for(int i = 1 ;i < int(word.size()) ; i++ ) {
+            if(word[i] == word[i - 1]) ans ++ ;
+        }
+
+        return ans;
+    }
+};
