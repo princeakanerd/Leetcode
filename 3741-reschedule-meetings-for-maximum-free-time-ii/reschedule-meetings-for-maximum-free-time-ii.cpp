c6f05dddd795c5333f047286f628490a53e5f8ca
Prince Gajnani <132270009+princeakanerd@users.noreply.github.com>
2025-07-10 13:10:02 +0530

    Time: 151 ms (20.32%) | Memory: 195.8 MB (15.94%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
+    vector<int> st ;
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
         int n = startTime.size() ;
-
-        vector<int> gap ;
         multiset<int> st ;
         
         for(int i = 0 ; i < n ;i++ ){
-            if(i == 0) gap.push_back(startTime[i]) ;
-            else gap.push_back(startTime[i] - endTime[i - 1]) ;
-            st.insert(gap.back()) ;
+            if(i == 0) st.insert(startTime[i]) ;
+            else st.insert(startTime[i] - endTime[i - 1]) ;
+            if(st.size() > 3) st.erase(st.begin()) ;
         }
 
-        gap.push_back(eventTime - endTime.back()) ;
         st.insert(eventTime - endTime.back()) ;
-
+        if(st.size() > 3) st.erase(st.begin()) ;
         int ans = 0 ;
+
         for(int i = 0 ; i < n ; i++){
-            int gap1 = gap[i] , gap2 = gap[i + 1] ;
-            st.erase(st.find(gap1));
-            st.erase(st.find(gap2));
-            int blocksize = endTime[i] - startTime[i] ;
-            int biggap = gap1 + gap2 + endTime[i] - startTime[i] ;
-            ans = max(ans , biggap - (*st.rbegin() >= blocksize ? 0 : blocksize));
-            st.insert(gap1) ;
-            st.insert(gap2) ;
+            int gap1 , gap2 ;
+            if(i == 0) gap1 = startTime[i] ;
+            else gap1 = startTime[i] - endTime[i - 1] ;
+
+            if(i == n - 1) gap2 = eventTime - endTime[i] ;
+            else gap2 = startTime[i + 1] - endTime[i] ;
+
+            int block = endTime[i] - startTime[i] ;
+            
+            bool g1 = false, g2 = false ;
+            if(st.find(gap1) != st.end()){
+                st.erase(st.find(gap1)) ;
+                g1 =true ;
+            }
+            if(st.find(gap2) != st.end()){
+                st.erase(st.find(gap2)) ;
+                g2 =true ;
+            }
+
+            if(*st.rbegin() >= block) ans = max(ans , block + gap1 + gap2) ;
+            else ans = max(gap1 + gap2, ans) ;
+
+            if(g1) st.insert(gap1) ;
+            if(g2) st.insert(gap2) ;
         }
         return ans;
     }
