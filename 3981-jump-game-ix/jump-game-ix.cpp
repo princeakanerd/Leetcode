13a1368779839ccf072abbb2d64621dc87643f76
Prince Gajnani <132270009+princeakanerd@users.noreply.github.com>
2026-05-07 16:45:25 +0530

    Time: 15 ms (47.34%) | Memory: 219.3 MB (85.64%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.cpp
+++ 3981-jump-game-ix/jump-game-ix.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size() ;
+        int maxi = 0 ;
+        vector<int> minarr(n) ;
+        int mini = INT_MAX ;
 
-        vector<int> pfxmax(n , nums[0]), sufmin(n ,nums.back()), ans(n) ;
-        for(int i = 1 ; i < n ; i++ ){
-            pfxmax[i] = max(pfxmax[i - 1] , nums[i]) ;
+        for(int i = n - 1 ;i >= 0; i-- ){
+            mini = min(mini, nums[i]) ;
+            minarr[i] = mini ;
         }
+        int idx = 0 ;
+        vector<int> res(n) ;
+        for(int i = 0 ;i < n ;i++ ){
+            maxi = max(maxi, nums[i]) ;
 
-        for(int i = n - 2; i >= 0 ; i-- ){
-            sufmin[i] = min(sufmin[i + 1] , nums[i]) ;
-        }   
-
-        ans[n - 1] = pfxmax[n - 1] ;
-
-        for(int i = n - 2 ; i >= 0 ; i-- ){
-            ans[i] = pfxmax[i] ;
-            if(pfxmax[i] > sufmin[i + 1]) {
-                ans[i] = max(ans[i], ans[i + 1]) ;
+            if(i < n - 1 && maxi <= minarr[i + 1]){
+                for(int j = i ; j >= idx ; j--){
+                    res[j] = maxi ;
+                }
+                idx = i + 1 ;
             }
         }
-        return ans ;
+
+        for(int j = n - 1 ; j >= idx ; j--){
+                    res[j] = maxi ;
+                }
+
+        return res;
         
     }
 };
